ef7637e5fb0edc0b0621e16bc31579150c6b6a95
Erik Barron <todayisagrnday21@gmail.com>
2020-01-28 14:23:51 -0800

    provides working movie input panel which adds movies to MovieList as they are entered. When done a separate button prompts the question for how many screening rooms there are. Still working on getting a selectable timetable to display

--- js/scripts.js
+++ js/scripts.js
@@ -22,11 +22,43 @@ function displayMovieList(movieListToDisplay) {
       "<div class='col-md-3'>" + movie.movieRating + "</div>" + 
       "<div class='col-md-6'>" + movie.movieSynopsis + "</div>" +
     "</div>"
-    
-    
-    // "<li><p><strong>" + movie.movieName + "</strong></p><br>" + "<p> Rated: " + movie.movieRating + "</p><br>" + "<p> Synopsis: " + movie.movieSynopsis + "</p></li>"
   });
   movieList.html(htmlForMovieList);
+};
+
+function generateTimetableForm(inputMovieDatabase, numberOfTheaters) {
+  var timetable = $("#timetableInput");
+  var htmlForTimetable = "";
+  var i = 1;
+  while ( i <= numberOfTheaters) {
+    htmlForTimetable += 
+    // "<div class='row'>" +
+      "<p><strong>Theater " + i + "</strong></p>" +
+      "<div class='row'>" +
+        "<div class='col-md-3'>" + 
+          "<p>12:00pm</p><br>" + 
+          + "<select>" + generateMovieSelector(inputMovieDatabase)  +
+          "</select>" +
+        "</div>" +
+        "<div class='col-md-3'>" + "3:00pm" + "</div>" +
+        "<div class='col-md-3'>" + "6:00pm" + "</div>" +
+        "<div class='col-md-3'>" + "9:00pm" + "</div>" +
+      "</div>";
+    // "</div>";
+    i++;
+  }
+  timetable.html(htmlForTimetable);
+}
+
+function generateMovieSelector(inputMovieDatabase) {
+  var htmlForSelector = "";
+  var localIndex = 0;
+  inputMovieDatabase.movies.forEach(function(movie) {
+    htmlForSelector += 
+      "<option value='" + localIndex + "'>" + movie.movieName + "</option>";
+  });
+
+  return htmlForSelector;
 };
 
 //Front End
@@ -35,20 +67,26 @@ var movieDatabase = new MovieList();
 $(document).ready(function() {
   $("form#inputForm").submit(function(event) {
     event.preventDefault();
-
     var inputtedMovieName = $("input#inputMovieName").val();
     var inputtedMovieRating = $("select#inputMovieRating").val();
     var inputtedMovieSynopsis = $("textarea#inputMovieSynopsis").val();
-
     $("input#inputMovieName").val("");
     $("select#inputMovieRating").val("NA");
     $("textarea#inputMovieSynopsis").val("");
-
     var newMovie = new Movie(inputtedMovieName, inputtedMovieRating, inputtedMovieSynopsis)
-
     movieDatabase.addMovie(newMovie);
-
     displayMovieList(movieDatabase);
+    $("#movieListDisplay").show();
+    });
 
-  })
-})
+  $("#doneEnteringMoviesButton").click(function() {
+    $("#inputMovieNameDisplay").hide();
+    $("#howManyTheatersPanel").show();
+  });
+
+  $("#submitHowManyTheaterPanelButton").click(function() {
+    var theaters = parseInt($("#inputHowManyTheaters").val());
+    generateTimetableForm(movieDatabase, theaters);
+  });
+
+});
